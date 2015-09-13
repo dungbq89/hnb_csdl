863594859da43d4d08fcb6c2dee2bdf8a14530e4
dungbq89 <dungbq89@gmail.com>
2015-09-13 16:28:49 +0700

    tai lieu nghiep vu

---
+++ apps/backend/modules/csdlTaiLieuNghiepVu/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/csdlTaiLieuNghiepVuGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/csdlTaiLieuNghiepVuGeneratorHelper.class.php';
+
+/**
+ * csdlTaiLieuNghiepVu actions.
+ *
+ * @package    Vt_Portals
+ * @subpackage csdlTaiLieuNghiepVu
+ * @author     ngoctv1
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class csdlTaiLieuNghiepVuActions extends autoCsdlTaiLieuNghiepVuActions
+{
+}
---
+++ apps/backend/modules/csdlTaiLieuNghiepVu/lib/csdlTaiLieuNghiepVuGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * csdlTaiLieuNghiepVu module configuration.
+ *
+ * @package    Vt_Portals
+ * @subpackage csdlTaiLieuNghiepVu
+ * @author     ngoctv1
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class csdlTaiLieuNghiepVuGeneratorConfiguration extends BaseCsdlTaiLieuNghiepVuGeneratorConfiguration
+{
+}
