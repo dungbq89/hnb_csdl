6206dfb62a3d27347ea8d5bfc0398dac05ea42fd
ngoctv <ngoctv84@gmail.com>
2015-09-19 20:27:00 +0700

    trang dang nhap

--- apps/backend/modules/sfGuardAuth/templates/_signin_form.php
+++ apps/backend/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,89 +1,261 @@
-<?php use_helper('I18N') ?>
 
-<?php if (count($errors=$form->getGlobalErrors()) > 0): ?>
-<div class="control-group error">
-    <ul class="error_list">
-    <?php foreach ($errors as $error): ?>
-        <li><?php echo __($error, null, 'tmcTwitterBootstrapPlugin') ?></li>
-    <?php endforeach; ?>
-    </ul>
-</div>
-<?php endif ?>
 
-<form class="form-horizontal" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
-    <?php echo $form->renderHiddenFields() ?>
-    <div class="control-group<?php echo $form['username']->hasError() ? ' error' : ''?>">
-        <?php echo $form['username']->renderLabel(null, array('class' => $form['username']->hasError() ? 'strong' : '')) ?>
-        <?php if ($form['username']->hasError()): ?>
-            <span class="help-block"><?php echo $form['username']->renderError() ?></span>
-	    <br />
-	    <br />
-        <?php endif ?>
-            
-        <?php echo $form['username']->render(array('class' => 'input-xlarge')) ?>
-    </div>
-    <div class="control-group<?php echo $form['password']->hasError() ? ' error' : ''?>">
-        <?php echo $form['password']->renderLabel(null, array('class' => $form['password']->hasError() ? 'strong' : '')) ?>
-        <?php if ($form['password']->hasError()): ?>
-            <span class="help-block"><?php echo $form['password']->renderError() ?></span>
-	    <br />
-        <?php endif ?>
-            
-        <?php echo $form['password']->render(array('class' => 'input-xlarge')) ?>
-    </div>
-  <?php if ($change_password == 1): ?>
-  <div class="control-group<?php echo $form['new_password']->hasError() ? ' error' : ''?>">
-    <?php echo $form['new_password']->renderLabel(__('Mật khẩu mới *', null, 'sf_guard'), array('class' => $form['new_password']->hasError() ? 'strong' : '')) ?>
-    <?php echo $form['new_password']->render(array('class' => 'input-xlarge')) ?>
-    <?php if ($form['new_password']->hasError()): ?>
-    <span class="help-block"><?php echo $form['new_password']->renderError() ?></span>
-    <?php endif ?>
-  </div>
-  <div class="control-group<?php echo $form['confirm_pass']->hasError() ? ' error' : ''?>">
-    <?php echo $form['confirm_pass']->renderLabel(__('Nhập lại mật khẩu mới *', null, 'sf_guard'), array('class' => $form['confirm_pass']->hasError() ? 'strong' : '')) ?>
-    <?php echo $form['confirm_pass']->render(array('class' => 'input-xlarge')) ?>
-    <?php if ($form['confirm_pass']->hasError()): ?>
-    <span class="help-block"><?php echo $form['confirm_pass']->renderError() ?></span>
-    <?php endif ?>
-  </div>
-  <?php endif ?>
-  <div class="control-group<?php echo $form['captcha']->hasError() ? ' error' : ''?>">
-        <?php echo $form['captcha']->renderLabel(null, array('class' => $form['captcha']->hasError() ? 'strong' : '')) ?>
-        <?php if ($form['captcha']->hasError()): ?>
-            <span class="help-block"><?php echo $form['captcha']->renderError() ?></span>
-	    <br />
-        <?php endif ?>
-            
-        <?php echo $form['captcha']->render(array('class' => 'input-xlarge')) ?>
-    </div>
-  
-    <?php if (isset($form['remember'])): ?>
-        <label class="checkbox">
-            <?php echo $form['remember']->render() ?>
-            <?php echo $form['remember']->renderLabelName() ?>
-        </label>
-    <?php endif; ?>
-    <input type="hidden" value="<?php echo $change_password ?>" name="change_password" />
-<!--    <input class="btn" type="submit" value="--><?php //echo __('Đăng Nhập', null, 'tmcTwitterBootstrapPlugin') ?><!--" />-->
-  <input class="btn" type="submit" value="<?php echo __(($change_password == 1) ? 'Đăng Nhập' : 'Đăng Nhập', null, 'tmcTwitterBootstrapPlugin') ?>" />
-  <?php if ($change_password == 1) : ?>
-    <input class="btn" type="button" value="<?php echo __('Hủy' , null, 'tmcTwitterBootstrapPlugin') ?>" href="<?php echo url_for('sf_guard_signin')?>" onclick="window.location ='<?php echo url_for('sf_guard_signin')?>'; "/>
-  <?php endif; ?>
-</form>
+<style type="text/css">
+    .content-login .ul-login h4 {
 
-<?php if (sfConfig::get('app_sfGuardPlugin_signin_links', false)): ?>
-    <div id="links">
-        <?php if (!include_slot('signin_links')): ?>
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: normal;
+        color: #0183cf;
+        margin-top: 10px;
+
+    }
+    .content-login .footer-login {
+        position: relative;
+        top: 230px;
+    }
+
+    .content-login .footer-login .login-ver {
+        height: 36px;
+        width: 66px;
+        position: relative;
+        left: 20px;
+        top: 40px;
+    }
+
+    .content-login .footer-login .conlogin .bq {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: bold;
+        color: #666666;
+        text-align: justify;
+        position: relative;
+        left: 250px;
+        top: 20px;
+    }
+
+    .content-login .footer-login .conlogin .bq1 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: normal;
+        color: #666666;
+        text-align: justify;
+        position: relative;
+        left: 210px;
+        top: 20px;
+    }
+
+    .content-login .footer-login .conlogin .bq2 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: normal;
+        color: #666;
+        visibility: inherit;
+        float: left;
+        width: 400px;
+        top: 20px;
+        position: relative;
+        left: 170px;
+    }
+
+</style>
+<![endif]-->
+
+<!--[if IE 7]>
+<style type="text/css">
+    .content-login .ul-login h4 {
+        clear: both !important;
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: normal;
+        color: #0183cf;
+        margin-top: 10px;
+        float: left;
+    }
+
+    .content-login .footer-login {
+        position: relative;
+        top: 230px;
+    }
+
+    .content-login .footer-login .login-ver {
+        height: 36px;
+        width: 66px;
+        position: relative;
+        left: 20px;
+        top: 40px;
+    }
+
+    .content-login .footer-login .conlogin .bq {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: bold;
+        color: #666666;
+        text-align: justify;
+        position: relative;
+        left: 250px;
+        top: 20px;
+    }
+
+    .content-login .footer-login .conlogin .bq1 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: normal;
+        color: #666666;
+        text-align: justify;
+        position: relative;
+        left: 210px;
+        top: 20px;
+    }
+
+    .content-login .footer-login .conlogin .bq2 {
+        font-family: Arial, Helvetica, sans-serif;
+        font-size: 11px;
+        font-weight: normal;
+        color: #666;
+        visibility: inherit;
+        float: left;
+        width: 400px;
+        top: 20px;
+        position: relative;
+        left: 170px;
+    }
+
+
+</style>
+<![endif]-->
+
+<!--[if IE 9]>
+<style type="text/css">
+    .content-login .footer-login .login-ver {
+        height: 36px;
+        width: 66px;
+        position: relative;
+        left: 20px;
+        top: -90px;
+    }
+
+</style>
+<![endif]-->
+
+<style type="text/css">
+    .img-dn {
+        float: left;
+        width: 50px;
+        margin-top: 110px;
+
+        border-top-style: none;
+        border-right-style: none;
+        border-bottom-style: none;
+        border-left-style: none;
+    }
+    .container-dn .img-bg .parent {
+        float: left;
+        width: 900px;
+        margin-left:200px;
+    }
+    .img-dn a img {
+        border-top-style: none;
+        border-right-style: none;
+        border-bottom-style: none;
+        border-left-style: none;
+    }
+    /* MESSAGES --------------------------------- */
+    .info, .errors, .success {margin:0px 0; padding:0px 0px 0px 0px; font-size:10px; line-height:1.5;}
+    .info {border:1px dotted 008;    background:#eff url(../images/info.gif) no-repeat 20px 18px; color:#008;}
+    .errors {border:1px dotted #d21033; background:#fed url(../images/error.gif) no-repeat 0px 1px; color:#d21033; padding-bottom: 0px;}
+    .success {border:1px dotted #390; background:#dfa url(../images/confirm.gif) no-repeat 20px 18px; color:#390;}
+    #content .errors h2, #content .success h2 {font-family:Georgia,"Times New Roman",Times,serif; font-size:18px; line-height:48px; font-weight:400; margin:0 0px 0 0; padding:0;}
+    #content .success h2 {color: #008 !important;}
+    #content .errors h2 {color:#b00 !important;}
+    #content .success h2 {color:#060 !important;}
+
+</style>
+
+<!-- script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script -->
+<!-- script type="text/javascript" src="js/common_rosters.js"></script -->
+<!-- link href="vina.css" rel="stylesheet" type="text/css" / -->
+<script type="text/javascript">
+    function formalizeSubs() {
+        var subs = document.getElementById('username').value;
+        if (subs.substring(0,1)=='0') document.getElementById('username').value = '84'+subs.substring(1);
+    }
+    function checkField(){
+        var u = document.getElementById('username').value;
+        var p = document.getElementById('password').value;
+        if(u == "" || p ==  "")
+        {
+            alert("Mời bạn nhập số thuê bao và mật khẩu!");
+            document.getElementById('username').focus();
+            return false;
+        }else{
+            return true;
+        }
+
+    }
+</script>
+
+
+<body id="cas" onload="init();">
+
+<div class="content-login">
+    <div class="ul-login">
+
+        <form id="fm1" class="fm-v clearfix" action="<?php echo url_for('@sf_guard_signin') ?>" method="post" >
+            <?php echo $form->renderHiddenFields() ?>
             <ul>
-                <?php if (isset($routes['sf_guard_forgot_password'])): ?>
-                    <li><a href="<?php echo url_for('@sf_guard_forgot_password') ?>"><?php echo __('Quên Mật Khẩu?', null, 'tmcTwitterBootstrapPlugin') ?></a></li>
-                <?php endif; ?>
-                <?php if (isset($routes['sf_guard_register'])): ?>
-                    <li><a href="<?php echo url_for('@sf_guard_register') ?>"><?php echo __('Want to register?', null, 'tmcTwitterBootstrapPlugin') ?></a></li>
-                <?php endif; ?>
-            </ul>
-        <?php endif ?>
-        <div class="clear"></div>
+                <li><span>Tên đăng nhập</span>
+
+
+                    <?php if ($form['username']->hasError()): ?>
+                        <span class="help-block"><?php echo $form['username']->renderError() ?></span>
+                        <br />
+                        <br />
+                    <?php endif ?>
+
+                    <?php echo $form['username']->render(array('class' => 'number-login')) ?>
+
+                </li>
+                <li><span>Mật khẩu</span>
+
+                    <?php if ($form['password']->hasError()): ?>
+                        <span class="help-block"><?php echo $form['password']->renderError() ?></span>
+                        <br />
+                    <?php endif ?>
+
+                    <?php echo $form['password']->render(array('class' => 'number-login')) ?>
+
+                </li>
+                <li><span>Mã xác thực</span>
+
+                    <?php if ($form['captcha']->hasError()): ?>
+                        <span class="help-block"><?php echo $form['captcha']->renderError() ?></span>
+                        <br />
+                    <?php endif ?>
+
+                    <?php echo $form['captcha']->render(array('class' => 'number-login')) ?>
+                </li>
+                <!--<li><span>Mã bảo vệ</span><input type="text" name="text" class="number-login1" /><div class="capcha"><h2>a4rt56</h2></div></li>-->
+                <li>
+                    <input name="_eventId" value="submit" type="hidden">
+                    <input name="dnstb" value="Đăng nhập" class="login-vn" onclick="return checkField()" type="submit">
+
+                    <input name="dnstb" value="Hủy" class="login-vn1" type="reset">
+
+                </li></ul>
+        </form>
+        <br>
+        <br>
+        <h3>Nếu chưa là thành viên, <a href="http://hoinhabaohatinh.vn/dang-ky">bấm vào đây</a> để đăng ký.</h3>
+    </div><!--end .ul-login-->
+    <div class="footer-login">
+
+        <div class="conlogin">
+            <div class="bq"</div>
+            <div class="bq1"></div>
+            <div class="bq2"></div>
+        </div>
     </div>
-    <?php
- endif ?>
+</div>
+
+</body></html>
