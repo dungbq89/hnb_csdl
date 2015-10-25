34b256434be12514e18138b533824363b646a7d4
dungbq89 <dungbq89@gmail.com>
2015-10-25 10:22:29 +0700

    qua trinh cong tac

---
+++ apps/backend/modules/csdlQuatrinhcongtac/templates/_list_th_tabular.php
@@ -0,0 +1,26 @@
+    <?php slot('sf_admin.current_header') ?>
+        <th  class="sf_admin_foreignkey sf_admin_list_th_hoivien_id" style="text-align: center">
+            <?php echo __('Hội viên', array(), 'messages') ?>
+        </th>
+    <?php end_slot(); ?>
+    <?php include_slot('sf_admin.current_header') ?>    <?php slot('sf_admin.current_header') ?>
+        <th  class="sf_admin_text sf_admin_list_th_thoigian" style="text-align: center">
+            <?php echo __('Thời gian (tháng)', array(), 'messages') ?>
+        </th>
+    <?php end_slot(); ?>
+    <?php include_slot('sf_admin.current_header') ?>    <?php slot('sf_admin.current_header') ?>
+        <th  class="sf_admin_date sf_admin_list_th_batdau" style="text-align: center">
+            <?php echo __('Bắt đầu', array(), 'messages') ?>
+        </th>
+    <?php end_slot(); ?>
+    <?php include_slot('sf_admin.current_header') ?>    <?php slot('sf_admin.current_header') ?>
+        <th  class="sf_admin_date sf_admin_list_th_ketthuc" style="text-align: center">
+            <?php echo __('Kết thúc', array(), 'messages') ?>
+        </th>
+    <?php end_slot(); ?>
+    <?php include_slot('sf_admin.current_header') ?>    <?php slot('sf_admin.current_header') ?>
+        <th  class="sf_admin_text sf_admin_list_th_chucvu" style="text-align: center">
+            <?php echo __('Chức vụ', array(), 'messages') ?>
+        </th>
+    <?php end_slot(); ?>
+    <?php include_slot('sf_admin.current_header') ?>
